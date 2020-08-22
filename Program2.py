d1c3c5b803fb62ce8909b470941c460f02c646e4
girishsj11 <59494641+girishsj11@users.noreply.github.com>
2020-08-22 22:35:52 +0530

    Add files via upload

---
+++ Program2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 22:28:42 2020
+
+@author: giri
+"""
+
+'''
+Convert two lists into a dictionary
+'''
+
+keys = ['Ten', 'Twenty', 'Thirty','Fourtee']
+values = [10, 20, 30, 40]
+
+dictionary = {key:value for key,value in zip(keys,values)}
+print(dictionary)
