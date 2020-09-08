a48e7a577e11135ae7e18a8b056ce4ad2ec80300
girishsj11 <59494641+girishsj11@users.noreply.github.com>
2020-09-08 10:39:52 +0530

    Create Program7.py

---
+++ Program7.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 10:27:11 2020
+
+@author: gshambul
+"""
+
+'''
+
+Program will display the number of occurance count of each unique values which is present in list .
+
+'''
+
+
+def list_creation(user_list):
+    element_count = int(input("Enter the number of elements to be present in user list : "))
+    if(element_count>0):
+        for _ in range(1,element_count+1):
+            print("Enter {} element : ".format(_))
+            user_list.append(int(input()))
+    else:
+        print(" please provide us the element_count value more than zero ")
+        
+  
+    
+if __name__ == "__main__" :
+    user_list = list()
+    list_creation(user_list)
+    print("user input list is : ",user_list)
+    print("Element --> Occurence count")
+    for i in set(user_list):
+        print(i," --> ",user_list.count(i))
