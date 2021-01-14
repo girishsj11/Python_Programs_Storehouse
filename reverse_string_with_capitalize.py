106b8730d688283568293db1e7bfea2a945adc8f
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-01-14 21:02:34 +0530

    Create reverse_string_with_capitalize.py

---
+++ reverse_string_with_capitalize.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 20:53:39 2021
+
+@author: giri
+"""
+
+def main():
+    '''
+    Accepts a string from user 
+    
+    Returns
+    -------
+    reversing of each words in a complete string by capitalizing it
+
+    '''
+    string = str(input("Enter the string to make it reverse each words with capitalizing : "))
+    out_string = ''
+    
+    string_split = string.lower().split()
+    
+    for words in string_split:
+        empty = ''
+        for char in words:
+            empty = char+empty
+            
+        out_string += empty.capitalize()+' '
+        
+    print(out_string)
+
+
+if __name__ == "__main__":
+    main()
