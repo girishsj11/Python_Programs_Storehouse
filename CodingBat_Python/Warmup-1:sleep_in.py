24a89ede2872e4db0b72b4dd6399ebcf1f70ac81
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-01-24 21:09:09 +0530

    Add files via upload

---
+++ CodingBat_Python/Warmup-1:sleep_in.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 24 19:02:11 2021
+
+@author: giri
+"""
+
+'''
+Warmup-1 > sleep_in
+
+
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+'''
+
