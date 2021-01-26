3ec3e2d4d9985ae31e10b2a8d21a74f4b2eda972
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-01-26 12:08:22 +0530

    Add files via upload

---
+++ codesignal/firstDuplicate.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 11:55:22 2021
+
+@author: giri
+"""
+
+'''
+Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+Example
+
+For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+
+There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+
+For a = [2, 2], the output should be firstDuplicate(a) = 2;
+
+For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer a
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 105,
+1 ≤ a[i] ≤ a.length.
+
+[output] integer
+
+The element in a that occurs in the array more than once and has the minimal index for its second occurrence. If there are no such elements, return -1.
+
+'''
+
+
+
+def firstDuplicate(a):
+    a_dict = dict()
+    for index,value in enumerate(a):
+        if(a.count(value)>1):
+            a_dict[index]=value
+    #print(a_dict)
+    #print("Keys : ",a_dict.keys())
+    #print("values : ",a_dict.values())
+    values = list(a_dict.values())
+    for i in range(len(values)-1):
+        if(values[i]==values[i+1]):
+            return values[i]
+    return -1
+
+if __name__ == "__main__":
+    print(firstDuplicate([1,3,2,4,3,5,2]))
