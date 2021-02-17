2f35f04de70b6727723a299edd01b60a75f6ca01
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-02-17 16:42:55 +0530

    Create Program_9.py

---
+++ Program_9.py
@@ -0,0 +1,12 @@
+#Program to make the list with unique elements in it which means removing duplicates from it 
+
+#method 1
+
+'''
+
+We can make it by passing the list into set method , as we are aware of this set is unordered & unique elements data type
+
+'''
+
+def list_creation(in_list):
+  pass
