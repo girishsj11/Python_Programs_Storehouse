a62f92b92a758243770cd9e07b401a7a164a22a6
girishsj <59494641+girishsj11@users.noreply.github.com>
2021-03-16 11:05:57 +0530

    Update reverse_number_verification.py

--- reverse_number_verification/reverse_number_verification.py
+++ reverse_number_verification/reverse_number_verification.py
@@ -44,4 +44,4 @@ def palindrome(string):
     
 if __name__ == "__main__":
     string = str(input("Enter the string which you wanted check whether its palindrome in it : "))
-    print(palindrome(string))
+    palindrome(string)
